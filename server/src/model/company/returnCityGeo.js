d4dd2f706c39b0ec631579aa76de7e8067eda56c
Leonid Kudryashov <leoural66@yandex.ru>
2023-02-16 16:32:41 +0500

    geo

--- server/src/model/company/returnCityGeo.js
+++ server/src/model/company/returnCityGeo.js
@@ -10,5 +10,8 @@ module.exports = (city) => {
     let geo = GEO.GEO.find(item => item.name === city)
     // console.log(`GEO::: ${geo.coords}:::>  `, [geo.coords.lat, geo.coords.lon]); // test
 
+    if (!geo) {
+        return []
+    }
     return [Number(geo.coords.lat), Number(geo.coords.lon)]
 }
