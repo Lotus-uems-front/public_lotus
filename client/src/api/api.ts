30172b088490a9a39e6537a40236de149d0ff50b
Leonid Kudryashov <leoural66@yandex.ru>
2023-02-27 09:50:30 +0500

    правка URL к серверу

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,13 +1,15 @@
 import axios from 'axios'
 
 const port = '5000'
-const baseURL = `http://localhost:${port}/api/company/get_all_data`
+// const baseURL = `http://localhost:${port}/api/company/get_all_data`
+const baseURL = `https://test.public.lotus-uems.ru/api/company/get_all_data`
+
 const headers = { "Content-Type": "application/json" }
 
 
 export const companiesDataApi = {
-    async getCompanyData(inn:string){
-        const response = await axios.post(baseURL, {inn})
+    async getCompanyData(inn: string) {
+        const response = await axios.post(baseURL, { inn })
         const data = await response.data
         return data
     }
