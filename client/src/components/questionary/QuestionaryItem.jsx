0880ce389172d3d349a2dd36d2788803ec6c2c3b
Anna <anna.semenova.91@inbox.ru>
2023-03-02 20:13:10 +0500

    added some stuff

--- client/src/components/questionary/QuestionaryItem.jsx
+++ client/src/components/questionary/QuestionaryItem.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect } from 'react'
 import { Accordion, Badge, ListGroup } from 'react-bootstrap'
+import { equip } from '../lists/occupationTypesLists'
 import s from './styles/Questionary.module.css'
 
-export const QuestionaryItem = ({ questionaryItem, idx, id }) => {
+export const QuestionaryItem = ({ questionaryItem, id }) => {
   console.log(questionaryItem)
 
-  const equip = ['Емкости без внутренних устройств', 'Емкости с внутренними устройствами', 'Емкости с перемешивающими устройствами', 'Емкости для хранения']
+
 
   const returnFormatedData = (item) => {
-    if (typeof item.value === 'boolean' && item.value === true) {
+    if (typeof item.value === 'boolean' && item.value === true && item.information !== 'Емкости для хранения' && item.information !== '[object Object]') {
       return (
-        <ListGroup.Item as='li' className={s.description} >
+        <ListGroup.Item as='li' className={s.description}>
           <div className='ms-2 me-auto'>
             <div className='fw-bold'>{item.information}</div>
           </div>
@@ -22,7 +23,7 @@ export const QuestionaryItem = ({ questionaryItem, idx, id }) => {
     }
     if (item.information === 'ФИО руководителя') {
       return (
-        <ListGroup.Item as='li' className={s.description} >
+        <ListGroup.Item as='li' className={s.description}>
           <div className='ms-2 me-auto'>
             <div className='fw-bold'>{item.information}</div>
           </div>
@@ -33,7 +34,7 @@ export const QuestionaryItem = ({ questionaryItem, idx, id }) => {
 
     if (typeof item.value === 'string') {
       return (
-        <ListGroup.Item as='li' className={s.description} >
+        <ListGroup.Item as='li' className={s.description}>
           <div className='ms-2 me-auto'>
             <div className='fw-bold'>{item.information}</div>
           </div>
@@ -43,17 +44,18 @@ export const QuestionaryItem = ({ questionaryItem, idx, id }) => {
         </ListGroup.Item>
       )
     }
-
-    if (item.description === 'Емкости без внутренних устройств' || item.description === 'Емкости с внутренними устройствами' ||
-    item.description === 'Емкости с перемешивающими устройствами' ||  item.description === 'Емкости для хранения') {
+    if (equip.some((el) => el === item.description)) {
+      if(item.value.some(elem => elem))
       return (
-       <ListGroup.Item >
-          <h4>{item.description}:</h4>
+        <ListGroup.Item>
+          <ListGroup.Item variant='primary' style={{ marginBottom: '20px' }}>
+            {item.description && item.description}
+          </ListGroup.Item>
           {item.information.map((el, ind) => {
             if (item.value[ind]) {
               return (
-                <div className={`${s.description} ${s.mb_10}`}>
-                  <div className=''>
+                <div key={el.id} className={`${s.description} ${s.mb_10}`}>
+                  <div>
                     <div className='fw-bold'>{el}</div>
                   </div>
                   <Badge bg='primary' pill>
@@ -78,19 +80,9 @@ export const QuestionaryItem = ({ questionaryItem, idx, id }) => {
           <>
             {item.value && (
               <ListGroup as='ul' key={index} className={s.description}>
-                {/* <ListGroup.Item as='li' > */}
-                  {/* <>
-                    <div className='ms-2 me-auto'>
-                      <div className='fw-bold'>{item.information}</div>
-                    </div>
-                    <Badge bg='primary' pill>
-                      {returnFormatedData(item)}
-                    </Badge>
-                  </> */}
-                  {returnFormatedData(item)}
-                {/* </ListGroup.Item> */}
+                {returnFormatedData(item)}
               </ListGroup>
-            )}
+            )}{' '}
           </>
         ))}
       </Accordion.Body>
