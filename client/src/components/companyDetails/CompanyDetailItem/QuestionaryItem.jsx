44aad3d55a29f5a688e829e03c8352200a8340fe
Anna <anna.semenova.91@inbox.ru>
2023-04-13 16:35:20 +0500

    table

--- client/src/components/companyDetails/CompanyDetailItem/QuestionaryItem.jsx
+++ client/src/components/companyDetails/CompanyDetailItem/QuestionaryItem.jsx
@@ -5,12 +5,11 @@ import s from '../../../css/Questionary.module.css'
 import downloadFile from '../../../assets/downLoadFile'
 import MultiColumnTable from '../../../assets/MultyColumnTable/MultyColumnTable'
 
-
 export const QuestionaryItem = ({ questionaryItem, id, colNum }) => {
-  console.log(questionaryItem)
+  // console.log(questionaryItem)
 
   const download = (fileName, login, id) => {
-    (async () => {
+    ;(async () => {
       try {
         const result = await downloadFile(fileName, login, id) // ? Почему то в id передается полный путь до файла
 
@@ -23,7 +22,7 @@ export const QuestionaryItem = ({ questionaryItem, id, colNum }) => {
           link.remove()
         }
       } catch (err) {
-        console.log(`Ошибка при загрузке файла: `, err);
+        console.log(`Ошибка при загрузке файла: `, err)
       }
     })()
   }
@@ -54,8 +53,9 @@ export const QuestionaryItem = ({ questionaryItem, id, colNum }) => {
   //   }
   // }
   // console.log(inn)
+  // console.log(questionaryItem)
 
-  const filteredData = questionaryItem.data.filter(item => item.value);
+  const filteredData = questionaryItem.data.filter((item) => item.value)
 
   return (
     <Accordion.Item
@@ -67,16 +67,7 @@ export const QuestionaryItem = ({ questionaryItem, id, colNum }) => {
         {questionaryItem.title}
       </Accordion.Header>
       <Accordion.Body className={`${s.accordion_body}`}>
-      <MultiColumnTable data={filteredData} columns={colNum} download={download}/>
-        {/* <Table bordered>
-          {questionaryItem.data.map((item, index) => (
-            <tbody key={index}>
-              {item.value && (
-                <tr>{returnFormatedData(item)}</tr>
-              )}{' '}
-            </tbody>
-          ))}{' '}
-        </Table> */}
+        <MultiColumnTable data={filteredData} columns={colNum} download={download} qi={questionaryItem}/>
       </Accordion.Body>
     </Accordion.Item>
   )
